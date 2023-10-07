a583654ba64901de6bfab9de87b132322f33438f
JocelynVelarde <a01285780@itesm.mx>
2023-10-07 16:21:15 -0600

    grids for homepage

--- ometeotl/lib/screen/home.dart
+++ ometeotl/lib/screen/home.dart
@@ -1,46 +1,317 @@
 import 'package:flutter/material.dart';
-import 'package:ometeotl/constants.dart';
-import 'package:ometeotl/screen/login_screen.dart';
-import 'package:ometeotl/widgets/background_scaffold.dart';
-import 'package:ometeotl/widgets/logo_image.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ManageHomePage extends StatefulWidget {
+  const ManageHomePage ({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ManageHomePageState createState() => _ManageHomePageState();
+}
+
+class _ManageHomePageState extends State<ManageHomePage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return BackgroundScaffold(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const LogoImage(),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 40.0, bottom: 40.0, left: 50.0, right: 50.0),
-              child: GestureDetector(
-                onTap: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const SignUpScreen())),
-                child: Container(
-                  height: MediaQuery.of(context).size.width / 8,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                      paleteLightBlue,
-                      paleteBlue,
-                    ]),
-                    borderRadius: BorderRadius.all(Radius.circular(25)),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Get Started',
-                      style: smallerDefaultStyleWhite,
-                    ),
-                  ),
-                ),
+    return Container(
+      decoration: const BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+      Color.fromARGB(255, 199, 14, 14),
+      Color.fromARGB(255, 247, 201, 76)
+    ],
+    stops: [0, 1],
+    begin: AlignmentDirectional(0.87, -1),
+    end: AlignmentDirectional(-0.87, 1),
+  ),
+),
+      child: Scaffold(
+          key: scaffoldKey,
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_rounded,
+                color: Colors.white,
+                size: 30,
+              ),
+              onPressed: () async {
+                Navigator.of(context).pop();
+              },
+            ),
+            title: const Text(
+              'Maneja Tu Hogar',
+              style: TextStyle(
+                fontFamily: 'Outfit',
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
             ),
-          ],
-        ),
+            actions: const [],
+            centerTitle: false,
+            elevation: 2,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    primary: false,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x33000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SwitchListTile(
+                                value: true,
+                                onChanged: (newValue) async {},
+                                title: const Text(
+                                  'Recamara',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                tileColor: Theme.of(context).colorScheme.background,
+                                activeColor: Theme.of(context).primaryColor,
+                                dense: false,
+                                controlAffinity: ListTileControlAffinity.trailing,
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1, 0),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 10, 10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x33000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SwitchListTile(
+                                value: false,
+                                onChanged: (newValue) async {},
+                                title: const Text(
+                                  'Entrada',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                tileColor: Theme.of(context).colorScheme.background,
+                                activeColor: Theme.of(context).primaryColor,
+                                dense: false,
+                                controlAffinity: ListTileControlAffinity.trailing,
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1, 0),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 10, 10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x33000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SwitchListTile(
+                                value: true,
+                                onChanged: (newValue) async {},
+                                title: const Text(
+                                  'Oficina',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                tileColor: Theme.of(context).colorScheme.background,
+                                activeColor: Theme.of(context).primaryColor,
+                                dense: false,
+                                controlAffinity: ListTileControlAffinity.trailing,
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1, 0),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 10, 10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x33000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SwitchListTile(
+                                value: true,
+                                onChanged: (newValue) async {},
+                                title: const Text(
+                                  'Patio',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                tileColor: Theme.of(context).colorScheme.background,
+                                activeColor: Theme.of(context).primaryColor,
+                                dense: false,
+                                controlAffinity: ListTileControlAffinity.trailing,
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1, 0),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 10, 10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 8),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            boxShadow: const [
+                              BoxShadow(
+                                blurRadius: 3,
+                                color: Color(0x33000000),
+                                offset: Offset(0, 1),
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SwitchListTile(
+                                value: false,
+                                onChanged: (newValue) async {},
+                                title: const Text(
+                                  'TV',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                tileColor: Theme.of(context).colorScheme.background,
+                                activeColor: Theme.of(context).primaryColor,
+                                dense: false,
+                                controlAffinity: ListTileControlAffinity.trailing,
+                              ),
+                              const Align(
+                                alignment: AlignmentDirectional(1, 0),
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 10, 10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
       ),
     );
   }
