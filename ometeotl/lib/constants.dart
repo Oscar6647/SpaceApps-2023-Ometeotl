1c006a80c493a2b65b0992930aa5c9f34eedbf24
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-07 13:34:36 -0600

    Created constants.dart

---
+++ ometeotl/lib/constants.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// Stores all system-wide constants
+
+/* Widget config */
+const double standartSpacerHeight = 30;
+
+const double smallerSpacerHeight = 10;
+
+/* Text style config */
+
+const String fontFamily = "Manrope";
+
+const TextStyle defaultStyle = TextStyle(
+  fontFamily: fontFamily,
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 25,
+);
+const TextStyle smallerDefaultStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.w700,
+  color: Colors.black,
+  fontSize: 18,
+);
+
+const TextStyle substitleStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.w700,
+  color: Colors.black,
+  fontSize: 15,
+);
+
+const TextStyle bigTextStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  fontSize: 52,
+);
+
+/* Color configuration */
+
+const Color paleteBlue = Color(0xff1D3BF4);
+const Color paleteLightBlue = Color(0xff10CCDE);
