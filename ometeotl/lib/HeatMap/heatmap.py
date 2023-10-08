d6ce95be5064be53d2aed338839433eae29e9aa5
Alejandra Coeto <a01285221@tec.mx>
2023-10-08 17:41:56 -0600

    "heatmap"

--- ometeotl/lib/HeatMap/heatmap.py
+++ ometeotl/lib/HeatMap/heatmap.py
@@ -1,25 +1,28 @@
 import geopandas as gpd
 from shapely.geometry import Point
+from flask import Flask, request, jsonify, send_from_directory
+import matplotlib
+matplotlib.use('Agg')  # 'Agg' is a non-interactive backend
 import matplotlib.pyplot as plt
 
 class heatmaps:
     #The class initializes the dataframes of all three datasets and list of dfs for easy access
 
-    def __init__(self):
+    def __init__(self, file_precipitacion, file_inundacion, file_granizo):
         # filename = "atlas_de_riesgo_precipitacion.shp"
         # file = open(filename)
         print('loading data...')
 
-        try:
-            self.df_inundacion = gpd.read_file('http://127.0.0.1:5000/shapefile/atlas_de_riesgo_precipitacion.shp')
-        
-        except Exception as e:
-            print("ERROR LOADING")
-            print(e)
-        #self.df_precipitacion = gpd.read_file(file)
-        # self.df_granizo =  gpd.read_file('atlas_de_riesgo_granizo.shp')
-        # self.lista_df = [self.df_inundacion, self.df_precipitacion, self.df_granizo]
-    #Simple plot function
+        self.df_precipitacion = file_precipitacion
+        self.df_inundacion = file_inundacion
+        self.df_granizo = file_granizo
+        self.lista_df = [self.df_inundacion, self.df_precipitacion, self.df_granizo]
+        self.lista = ["Flooding", "Precipitation", "Hail"]
+        self.alcaldias = ["Álvaro Obregón", "Azcapotzalco", "Benito Juárez", "Coyoacán", "Cuajimalpa de Morelos", "Cuauhtémoc", "Gustavo A. Madero", "Iztacalco", "Iztapalapa", "La Magdalena Contreras", "Miguel Hidalgo", "Milpa Alta", "Tláhuac", "Tlalpan", "Venustiano Carranza" , "Xochimilco"]
+        self.warnings = ["very low", "low", "moderate", "high", "very high"]
+
+        print('done w data')
+
     
     def plot(self, num):
 
@@ -27,7 +30,13 @@ class heatmaps:
 
 
         # Create a larger figure
-        fig, ax = plt.subplots(figsize=(12, 12))
+        try:
+            fig, ax = plt.subplots(figsize=(12, 12))
+            print('subplot')
+        except Exception as e:
+            print('error')
+            print(e)
+            return
 
         # Plot the df_granizo data
         df.plot(column="int2", legend=True, legend_kwds={"label": "Intensidad de " +   self.lista_df[num]['taxonom'].iloc[0].lower(), "orientation": "horizontal"}, ax=ax)
@@ -44,7 +53,6 @@ class heatmaps:
         plt.show()
 
     def position(self, long, lat, num):
-
         df = self.lista_df[num]
 
 
@@ -52,21 +60,27 @@ class heatmaps:
         target_coordinate = Point(long, lat)
         result = df[df.contains(target_coordinate)]
 
+        print('position')
         target_gdf = gpd.GeoDataFrame(geometry=[target_coordinate], crs=df.crs)
 
         #Checar si es la alcaldia correcta
 
-        titulo = "El peligro por " + result['taxonom'].iloc[0].lower() + " en la alcaldia " + result['alcaldi'].iloc[0] + " es " + result['intnsdd'].iloc[0].lower()
+        # titulo = "El peligro por " + result['taxonom'].iloc[0].lower() + " en la alcaldia " + result['alcaldi'].iloc[0] + " es " + result['intnsdd'].iloc[0].lower()
+        titulo = "T______________titulo :)"
 
         # Create a larger figure
+
         fig, ax = plt.subplots(figsize=(12, 12))
 
         # Plot df data
         #Falta agregar un mejor tamaño de subtitulo
-        df.plot(column="int2", legend=True, legend_kwds={"label": "Intensidad de " +  result['taxonom'].iloc[0].lower(), "orientation": "horizontal"}, ax=ax)
+        # df.plot(column="int2", legend=True)
+        # df.plot(column="int2", legend=True, legend_kwds={"label": "Intensidad de " +  result['taxonom'].iloc[0].lower(), "orientation": "horizontal"}, ax=ax)
+        df.plot(column="int2", legend=True, legend_kwds={"label": "Intensidad de " , "orientation": "horizontal"}, ax=ax)
 
         # Plot the target point
         target_gdf.plot(ax=ax, markersize=100, color='red', marker='*', label='Your Label', zorder=3)
+        return
 
         ax.axis('off')
   
--- ometeotl/lib/flask_api.py
+++ ometeotl/lib/flask_api.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 from flask_cors import CORS
 from datetime import datetime, timedelta
 import pandas as pd
-import HeatMap.heatmap as HM
 import geopandas
+import HeatMap.heatmap as HM
+from io import BytesIO
+import requests
+import fiona
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for your Flask app
@@ -11,9 +16,19 @@ CORS(app)  # Enable CORS for your Flask app
 # Load your CSV data into a DataFrame
 data = pd.read_csv('Template_Pred.csv')
 
+path = 'atlas_de_riesgo_inundaciones'
+inundaciones = geopandas.read_file('atlas_de_riesgo_inundaciones')
+precipitaciones = geopandas.read_file('atlas_de_riesgo_precipitacion')
+granizo = geopandas.read_file('atlas_de_riesgo_granizo')
+
+print('done')
+# data2 = open('atlas_de_riesgo_inundaciones')
+
+
 @app.route('/', methods=['POST'])
 def make_prediction():
     try:
+        
         data = request.get_json()  # Parse incoming JSON data
 
         # Check if the required fields are present in the JSON data
@@ -51,36 +66,100 @@ def find_prediction(year, doy):
         return 'Prediction not found for the specified year and day'
     
 
-@app.route('/shapefile/<filename>')
-def serve_shapefile(filename):
-    return send_from_directory('static', filename, mimetype='application/x-qgis')
+# @app.route('/shapefile/<filename>')
+# def serve_shapefile(filename):
+#     return send_from_directory('static', filename)
 
-@app.route('/getHeatMap', methods=['POST'])
+# @app.route('/plot')
+# def plot():
+#     # Create a Matplotlib plot
+#     fig, ax = plt.subplots(figsize=(12, 12))
+#     ax.plot([1, 2, 3, 4])
+
+#     # Save the plot to a BytesIO buffer
+#     buffer = BytesIO()
+#     plt.savefig(buffer, format='png')
+#     buffer.seek(0)
+
+#     # Return the plot as a response
+#     return send_file(buffer, mimetype='image/png')
+
+@app.route('/getHeatMap', methods=['GET', 'POST'])
 def getHeatMap():
-    # data = request.get_json()
-    # print(data)
-    # return
-    try:
-        data = request.get_json()  # Parse incoming JSON data
 
-        if (data):
-            result = {
-                'message': 'Prediction successful',
-                'prediction': "Holi"
-            }
-            # geopandas.read_file('/HeatMap/atlas_de_riesgo_precipitacion.shp')
+    try:
+        datax = request.get_json()  # Parse incoming JSON data
+
+        if (datax is not None):
+            
             try:
-                file = open('shapefile/test.txt')
-                req = request.files.get('fileToUpload')
-                df_inundacion = geopandas.read_file('http://127.0.0.1:5000/shapefile/test.txt')
-                print("SUCCESS")
+                # file = geopandas.read_file(data2, encoding='utf-8')
+                # geopandas.io.file.fiona.drvsupport.supported_drivers['ESRI Shapefile']['Shx'] = 'YES'
+                # fileX = geopandas.read_file(data2, encoding='utf-8')
+                print('load')
+                heatMap = HM.heatmaps(precipitaciones, inundaciones, granizo)
+                print('done')
+                heatMap.position(19.4284700,-99.1276600,1)
+                print('done2')
+                # heatMap.plot(2)
+
+                # shapefile_path = '../shapefiles/atlas_de_riesgo_granizo.shp'
+                # shapefile = geopandas.read_file(shapefile_path)
+                # # Read the shapefile using GeoPandas
+                # shapefile = geopandas.read_file(shapefile_path)
+                
+                # url = "http://127.0.0.1:5000/shapefile/atlas_de_riesgo_precipitacion.shp"
+                # # req1 = request.files.get('http://127.0.0.1:5000/shaperfile/atlas_de_riesgo_inundaciones/atlas_de_riesgo_inundaciones')
+                # response = requests.get(url)
+                # content = response.content
+                # # gdf = geopandas.read_file(geopandas.io.file.fiona.BytesCollection(content))
+
+                # file = geopandas.read_file(file)
+                # print('doneeee')
+                # file = open("http://127.0.0.1:5000/shapefile/atlas_de_riesgo_precipitacion.shp")
+                # shapefile = geopandas.read_file("http://127.0.0.1:5000/shapefile/atlas_de_riesgo_precipitacion.shp")
+               
+                # # file = open('shapefile/test.txt')
+                # req2 = request.files.get('http://127.0.0.1:5000/shapefile/atlas_de_riesgo_inundacion.shp')
+                # req3 = request.files.get('http://127.0.0.1:5000/shapefile/atlas_de_riesgo_granizo.shp')
+                
+                # url = 'http://127.0.0.1:5000/shaperfile/atlas_de_riesgo_precipitacion.shp'
+                # response = request.files.get(url)
+
+                # if response:
+                #     # The file has been successfully downloaded
+                #     file_content = response
+                    
+                #     # You can now process the file content, for example, save it to a local file or open it for reading
+                #     with open('local_shapefile.shp', 'wb') as local_file:
+                #         local_file.write(file_content)
+                # else:
+                #     print(f"Failed to download the file. Status code: {response}")
+                # t = "aaa"
+                
+                # if (req1 is not None):
+                #     print("SUCCESS")
+                #     # df_precipitacion = geopandas.read_file(req1)
+                #     # df_inundacion = geopandas.read_file(req2)
+                #     # df_granizo = geopandas.read_file(req3)
+                #     # heatMap = HM.heatmaps(df_precipitacion, df_inundacion, df_granizo)
+                #     # heatMap.position(19.4284700,-99.1276600,0)
+                #     # heatMap.plot(2)
+                #     # return jsonify(r={'attem':'done'}), 200
+                    
+
+                result = {
+                'message': 'Prediction successful',
+                'prediction': 'a'
+                }
+                
             except Exception as e:
                 print("ERROR LOADING")
                 print(e)
 
 
             
-            # heatMap = HM.heatmaps()
+            
         # # Check if the required fields are present in the JSON data
         # if 'longitud' in data and 'latitud' in data:
         #     v = -99.1276600
@@ -100,4 +179,4 @@ def getHeatMap():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=False)
